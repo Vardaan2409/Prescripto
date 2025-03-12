75e42732c951a21e6221161e6ff392a85088a08b
Vardaan2409 <madhisiya.vardan24@gmail.com>
2025-03-12 16:49:25 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import mongoose from 'mongoose';
 
 //app config
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 connectDB();
 connectCloudinary();
 
