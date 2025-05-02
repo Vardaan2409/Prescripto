9bcbbe6c5c60857ab29bcb2bd86ca363cfaab9f6
Vardaan Madhisiya <madhisiya.vardan24@gmail.com>
2025-05-03 00:58:42 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import doctorRouter from "./routes/doctorRoute.js";
 import userRouter from "./routes/userRoute.js";
 import mongoose from 'mongoose';
 
+dotenv.config();
+
 //app config
 const app = express();
 const port = process.env.PORT || 5000;
