511dc81673b598a8447eb0fdbf2c45fc057a38dd
Vardaan Madhisiya <madhisiya.vardan24@gmail.com>
2025-06-24 12:39:50 +0530

    minor_changes

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -51,7 +51,7 @@ const Sidebar = () => {
 
                     <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`} to={"/doctor-profile"}>
                         <img src={assets.people_icon} alt='' />
-                        <p className='hidden md:block'>Doctors List</p>
+                        <p className='hidden md:block'>Doctor's Profile</p>
                     </NavLink>
                 </ul>
             }
