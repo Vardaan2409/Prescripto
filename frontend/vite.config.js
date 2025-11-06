b66bc34a4d8a85a717a1eb08cb5bc13da45c0dc2
Vardaan Madhisiya <madhisiya.vardan24@gmail.com>
2025-11-06 14:56:02 +0530

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,15 @@
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:5173}
-},
-build: {
-    outDir: 'build'
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: 'build',
+  },
 })
